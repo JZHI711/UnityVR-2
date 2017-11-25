e3c222b580e7ec9d47534bfc4aef07399c42d78b
4419-2-8\user <s1410422032@gms.nutc.edu.tw>
2017-11-25 14:45:30 +0800

    5.clip

--- Assets/Scripts/ShootingGalleryController.cs
+++ Assets/Scripts/ShootingGalleryController.cs
@@ -14,6 +14,11 @@ public class ShootingGalleryController : MonoBehaviour//Class類別
     public Image timerBar;
     public float gameDuration=30f;
     public float endDelay =1.5f;
+
+    public Collider spawnCollider;
+    public ObjectPool targetObjectPool;//物件池(一次產生所有物件可以重複使用)
+    public float spawnProbablility=0.7f;//產生機率
+    public float spawnInterval=1f;//間隔時間
     public bool IsPlaying//Property屬性  判斷遊戲正在玩還是結束(只有自己可以更改，外部只能讀取)
     {
         private set;
@@ -52,10 +57,20 @@ public class ShootingGalleryController : MonoBehaviour//Class類別
         reticle.Show();
         SessionData.Restart();
         float gameTimer = gameDuration;
+        float spawnTimer = 0f;
         while (gameTimer > 0f)
         {
+            if (spawnTimer <= 0f)
+            {
+                if (Random.value < spawnProbablility)
+                {
+                    spawnTimer = spawnInterval;//reset下一次的時間
+                    Spawn();
+                }
+            }
             yield return null;//停 一 ㄓㄣ-
             gameTimer -= Time.deltaTime;
+            spawnTimer -= Time.deltaTime;
             timerBar.fillAmount = gameTimer / gameDuration;
         }
         IsPlaying = false;
@@ -68,5 +83,22 @@ public class ShootingGalleryController : MonoBehaviour//Class類別
         yield return new WaitForSeconds(endDelay);
         yield return StartCoroutine(selectionRadial.WaitForSelectionRadialToFill());
         yield return StartCoroutine(uiController.HideOutroUI());
+    }
+
+    private void Spawn()
+    {
+        GameObject target = targetObjectPool.GetGameObjectFromPool();
+        target.transform.position = SpawnPosition();
+    }
+    private Vector3 SpawnPosition() //不會回傳值打void;回傳三維向量要打Vector3
+    {
+        Vector3 center = spawnCollider.bounds.center;//碰撞體的中心點
+        Vector3 extents = spawnCollider.bounds.extents;//不論長寬高都是回傳一半的值
+        float x = Random.Range(center.x - extents.x,center.x + extents.x);
+        float y = Random.Range(center.y - extents.y,center.y + extents.y);
+        float z = Random.Range(center.z - extents.z,center.z + extents.z);
+        return new Vector3(x, y, z);
+
+
     }
 }
--- Assets/Scripts/ShootingTarget.cs
+++ Assets/Scripts/ShootingTarget.cs
@@ -7,5 +7,90 @@ using VRStandardAssets.Common;
 
 public class ShootingTarget : MonoBehaviour
 {
+    public int score = 1;
+    public float destroyTimeOutDuration = 2f;
+    public event Action<ShootingTarget> onRemove;//event Action<事件必須符合的參數> 名稱;
+    public float timeOutDuration = 2f;
+    private Transform cameraTransform;
+    private AudioSource audioSource;
+    private VRInteractiveItem vrInteractiveItem;
+    private Renderer mRenderer;
+    private Collider mCollider;
+    public AudioClip destroyClip;//音效
+    public GameObject destroyPrefab;
+    private bool isEnding;
+    public AudioClip spawnClip;
+    public AudioClip missedClip;
 
+    private void Awake()
+    {
+        cameraTransform = Camera.main.transform;
+        audioSource = GetComponent<AudioSource>();
+        vrInteractiveItem = GetComponent<VRInteractiveItem>();
+        mRenderer = GetComponent<Renderer>();
+        mCollider = GetComponent<Collider>();
+    }
+    private void OnEnable()
+    {
+        vrInteractiveItem.OnDown +=HandleDown;
+    }
+    private void OnDisable()
+    {
+        vrInteractiveItem.OnDown -= HandleDown;
+    }
+    private void OnDestroy()
+    {
+        onRemove = null;
+    }
+    private void HandleDown()
+    {
+        StartCoroutine(OnHit());
+    }
+    private IEnumerator OnHit()
+    {
+        if (isEnding)
+        yield break;
+        isEnding = true;
+        mRenderer.enabled = false;
+        mCollider.enabled = false;
+        audioSource.clip = destroyClip;
+        audioSource.Play();
+        SessionData.AddScore(score);
+        GameObject desroyedTarget = Instantiate<GameObject>(destroyPrefab, transform.position, transform.rotation);//Instantiate實例化物件
+        Destroy(desroyedTarget, destroyTimeOutDuration);
+        yield return new WaitForSeconds(destroyClip.length);
+        if (onRemove!= null)
+        {
+            onRemove(this);//因為上面的Action<ShootingTarget> onRemove;
+        }
+    }
+    public void Restart()
+    {
+        mRenderer.enabled = true;
+        mCollider.enabled = true;
+        isEnding = false;
+        audioSource.clip = spawnClip;
+        audioSource.Play();
+        transform.LookAt(cameraTransform.position);
+        StartCoroutine(MissTarget());
+        //StartCoroutine(GameOver());
+
+    }
+
+    private IEnumerator MissTarget()
+    {
+        yield return new WaitForSeconds(timeOutDuration);
+        if (isEnding)
+            yield break;
+        isEnding = true;
+        mRenderer.enabled = false;
+        mCollider.enabled = false;
+        audioSource.clip = missedClip;
+        audioSource.Play();
+        yield return new WaitForSeconds(missedClip.length);
+        if (onRemove != null)
+        
+            onRemove(this);
+        
+    }
 }
